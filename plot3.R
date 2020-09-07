fdfe542269acca3b5a479397827bcf8390abfd8c
KTezel <66541310+KTezel@users.noreply.github.com>
2020-09-06 20:03:40 -0700

    Plot 3 PNG margins corrected

--- plot3.R
+++ plot3.R
@@ -27,13 +27,14 @@ consumption_data <- read.csv.sql("household_power_consumption.txt","select * fro
 ## Combine date and time
 consumption_data$DateTime <- strptime(paste(consumption_data$Date, consumption_data$Time), format = "%d/%m/%Y %H:%M:%S")
 
+## Create a png object
+png("plot3.png", 480,480)
 
-## Plot 2
+## Plot 3
 plot(consumption_data$DateTime, consumption_data$Sub_metering_1, type = "l", ylab = "Energy Submetering", xlab = "")
 lines(consumption_data$DateTime, consumption_data$Sub_metering_2, type = "l", col = "red")
 lines(consumption_data$DateTime, consumption_data$Sub_metering_3, type = "l", col = "blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, lwd = 2.5, col=c("black", "red", "blue"))
 
 ## Copy plot as a PNG file
-dev.copy(png, file = "plot3.png", width = 489, height = 480)
 dev.off()
